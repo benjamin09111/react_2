6fbc2e330b8e91454350eb37262ccd8f09201b1b
Benjamin <benjamin.morales3@mail.udp.cl>
2024-01-16 03:09:01 -0300

    advance

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import {images} from "../../constants";
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div  className='app__aboutus app__bg flex__center section__padding'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt="g letter" />
+    </div>
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About us</h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+        <p className='p__opensans'>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+        <p className='p__opensans'>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+    </div>
   </div>
 );
 
--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { SubHeading } from "../../components"
+import { images } from "../../constants"
 
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef´s Word"/>
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>
+        <p className='p__opensans'>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+
+    </div>
   </div>
 );
 
